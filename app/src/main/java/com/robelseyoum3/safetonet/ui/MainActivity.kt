77519d3ddff32f486f7c1665e9790717fec6470f
Robel <robelseyoum3@gmail.com>
2020-01-09 14:57:28 +0000

    Adding this refresh activity with swipeRefreshLayout

--- app/src/main/java/com/robelseyoum3/safetonet/ui/MainActivity.kt
+++ app/src/main/java/com/robelseyoum3/safetonet/ui/MainActivity.kt
@@ -3,9 +3,13 @@ package com.robelseyoum3.safetonet.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
 import com.robelseyoum3.safetonet.R
 import com.robelseyoum3.safetonet.di.DaggerAppComponent
 import com.robelseyoum3.safetonet.di.NetworkModule
@@ -22,9 +26,19 @@ class MainActivity : AppCompatActivity() {
     lateinit var rocketViewModelFactory: RocketViewModelFactory
     lateinit var viewModel: RocketViewModel
 
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //here check if this field is true or false
+        //true == welcome page else
+
+
+        //getting swipeRefreshLayput from xml
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
 
         //for dependency injection
         getDependencies()
@@ -37,7 +51,9 @@ class MainActivity : AppCompatActivity() {
 
         //desplay the rocket data on RecycleView through adaptor
         viewModel.returnAllRocketsResult().observe(this,
-            Observer<List<Rockets>> { t ->  allRocketAdapter(t) })
+            Observer<List<Rockets>> {
+                    t ->  allRocketAdapter(t)
+            })
 
         //checking error while fetching and displaying data
         viewModel.returnError().observe(this, Observer {
@@ -60,15 +76,25 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        //Here refreshing the page, when user swipe
+        swipeRefreshLayout.setOnRefreshListener {
+
+            viewModel.getAllRocketsData()
+
+            Toast.makeText(this, "Swipe called", Toast.LENGTH_SHORT).show()
+
+            swipeRefreshLayout.isRefreshing = false
+
+        }
+
 
     }
 
     private fun allRocketAdapter(t: List<Rockets>) {
 
-        val adaptor = RocketAdaptor(t)
+        var adaptor = RocketAdaptor(t)
         rvList.layoutManager = LinearLayoutManager(this)
         rvList.adapter = adaptor
-
     }
 
     private fun getDependencies() {
